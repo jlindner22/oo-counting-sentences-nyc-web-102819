94ddba694f85622f164caa327776cdd597cbf13d
Jen Lindner <jenlindner22@gmail.com>
2019-10-29 23:29:22 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,10 +3,10 @@ require 'pry'
 class String
 
   def sentence?
- if self.String.include?(".") == true
- else
-   false
-  # "Hi, my name is Sophie.".include?(".") == true
+# if self.String.include?(".") == true
+# else
+#   false
+  "Hi, my name is Sophie.".include?(".") == true
   # "Hi, my name is Sophie.".include?("!") == false
   end
 
